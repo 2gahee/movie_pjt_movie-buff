40642cd2cd7cc4bcb17ef468bb0d6a691e22813f
te04072 <te04072@gmail.com>
2024-11-20 11:33:47 +0900

    CORS issue resolved

--- vue-pjt/src/stores/counter.js
+++ vue-pjt/src/stores/counter.js
@@ -2,16 +2,23 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import axios from 'axios'
 export const useMovieStore = defineStore('movie', () => {
- //여러개의 컴포넌트에서 활용하는 데이터만 store로 관리
+  const token = 'Token b63f63cb8d56de105c3e5b5c526686222ab0917a'
   const articles = ref([])
   const API_URL = 'http://127.0.0.1:8000'
   const getArticles = function () {
     axios({
       method: 'get',
-      url: `${API_URL}/community`
+      url: `${API_URL}/community`,
+      headers: {
+        Authorization: token,
+        Accept: 'application/json'
+      },
+      withCredentials: true
     })
     .then(res => {
+      articles.value = res.data
       console.log(res.data)
+      
     })
     .catch(err => console.log(err))
   }
