f45e61d52ab520a0e725ee76135197a03e425c94
이가희 <lgh7956@naver.com>
2024-11-25 02:24:53 +0900

    레이아웃 수정

--- vue-pjt/src/components/AccountArticles.vue
+++ vue-pjt/src/components/AccountArticles.vue
@@ -5,8 +5,8 @@
         <table class="table table-hover table-striped text-center">
             <thead>
                 <tr>
-                    <th class="title col-3"><a>제목</a></th>
-                    <th class="content col-4"><a>내용</a></th>
+                    <th class="title col-5"><a>제목</a></th>
+                    <!-- <th class="content col-4"><a>내용</a></th> -->
                     <th class="created-at"><a>작성일</a></th>
                 </tr>
             </thead>
@@ -14,7 +14,7 @@
                 <!-- 현재 페이지에 해당하는 게시글만 표시 -->
                 <tr v-for="article in paginatedArticles" :key="article.id">
                     <td class="title"><RouterLink :to="{ name: 'articleDetail', params: { id: article.id } }">{{ article.title }}</RouterLink></td>
-                    <td class="content">{{ article.content }}</td>
+                    <!-- <td class="content">{{ article.content }}</td> -->
                     <td class="created-at"><time>{{ article.created_at }}</time> </td>
                 </tr>
             </tbody>
@@ -80,22 +80,22 @@ const goCreate = function() {
   margin-left: 3rem;    
 }
 
-table {
- width:80%;
- margin: 0 auto;
-}
-
-
 .btn-table {
+ margin-top: 3%;
  display: flex;
  flex-direction: column;
- align-items: flex-end;
+ align-items: center;  
+ width: 70%; 
+ margin-left: auto; 
+ margin-right: auto; 
+ margin-bottom: 4rem;
 }
+
 
 .btn {
 width: 6rem;
-margin-right: 10%;
 margin-bottom: 1rem;
+align-self: flex-end; 
 }
 
 #pagination{
